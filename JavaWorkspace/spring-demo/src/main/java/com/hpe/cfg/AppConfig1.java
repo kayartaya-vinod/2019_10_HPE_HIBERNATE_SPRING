f0061c5106acfa72a2fe93494323e63a2f4aa038
kayartaya-vinod <vinod@vinod.co>
2019-10-22 16:09:41 +0530

    🇮🇳

--- JavaWorkspace/spring-demo/src/main/java/com/hpe/cfg/AppConfig1.java
+++ JavaWorkspace/spring-demo/src/main/java/com/hpe/cfg/AppConfig1.java
@@ -1,6 +1,9 @@
 package com.hpe.cfg;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 
 import com.hpe.dao.ProductDao;
 import com.hpe.dao.ProductDaoDummyImpl;
@@ -8,20 +11,27 @@ import com.hpe.dao.ProductDaoJdbcImpl;
 
 // Spring uses this class to load beans (objects) into 
 // the spring container.
+@Configuration
 public class AppConfig1 {
 	
 	public AppConfig1() {
 		System.out.println("AppConfig1 instantiated!");
 	}
-
+	
+	@Lazy	// effective only for singleton scoped bean
 	@Bean
 	public ProductDao jdbc() {
-		System.out.println("AppConfig1.jdbc() called!");
+		System.out.println("AppConfig1.jdbc() called! this is an instanceof " + this.getClass());
 		return new ProductDaoJdbcImpl();
 	}
 
+	@Scope("prototype")
 	@Bean
 	public ProductDao dummy() {
+		for(int i=0; i<10; i++) {
+			System.out.println("Inside loop, i is: " + i);
+			jdbc();
+		}
 		System.out.println("AppConfig1.dummy() called!");
 		return new ProductDaoDummyImpl();
 	}
--- JavaWorkspace/spring-demo/src/main/java/com/hpe/programs/P01SpringAsFactoryOfBeans.java
+++ JavaWorkspace/spring-demo/src/main/java/com/hpe/programs/P01SpringAsFactoryOfBeans.java
@@ -11,10 +11,14 @@ public class P01SpringAsFactoryOfBeans {
 	public static void main(String[] args) throws DaoException {
 		// spring container that loads beans from an annotation based config class
 		AnnotationConfigApplicationContext ctx;
-		
 		ctx = new AnnotationConfigApplicationContext(AppConfig1.class);
+		System.out.println("-------------pass 1");
+		
 		
 		ProductDao dao = ctx.getBean("dummy", ProductDao.class);
+		// ProductDao dao2 = ctx.getBean("dummy", ProductDao.class);
+		
+		// System.out.println("dao==dao2 is " + (dao==dao2));
 		long pc = dao.count();
 		System.out.println("There are " + pc + " products");
 
